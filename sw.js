5a07e12d0e04ec1216cf3bf2d52cd1a093c10465
muhiuddinalam <61884073+muhiuddinalam@users.noreply.github.com>
2026-02-03 20:13:14 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,12 @@
 // SteelSolver Service Worker
-const CACHE_NAME = 'steelsolver-' + new Date().getTime();
-
-self.addEventListener('install', function(event) {
-  console.log('SteelSolver PWA: Installing');
-  event.waitUntil(self.skipWaiting());
+self.addEventListener('install', function(e) {
+  e.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('SteelSolver PWA: Activating');
-  event.waitUntil(self.clients.claim());
+self.addEventListener('activate', function(e) {
+  e.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', function(event) {
-  // Basic fetch handler
-  event.respondWith(
-    fetch(event.request)
-      .catch(function() {
-        // If offline, you could return cached response
-        return new Response('SteelSolver is offline. Please check your connection.', {
-          headers: { 'Content-Type': 'text/plain' }
-        });
-      })
-  );
+self.addEventListener('fetch', function(e) {
+  e.respondWith(fetch(e.request));
 });
